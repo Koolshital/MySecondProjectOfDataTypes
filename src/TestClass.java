1e64089645a87542620a3fa60f2fbbd9175ec67e
Koolshital <koolshital@yahoo.com>
2023-04-21 11:36:45 +0100

    testclass

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,9 +1,16 @@
 public class TestClass {
     public static void findFactorial(int n){
+        int result =1;
+        if(n>0){
+            for(int i =n;i>=1;i--){//or (int i=1;i<=n;i++)
+                result = result*i;
 
-
-
-
+            }
+            System.out.println(result);
+        }
     }
-
+    public static void main(String[] args) {
+        findFactorial(3);
+        findFactorial(5);
+    }
 }
